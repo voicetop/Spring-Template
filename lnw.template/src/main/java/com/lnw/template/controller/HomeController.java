95c61b7a90a4074079f46a98c8d6ca0d8048a4a6
voicetop <interalia87@gmail.com>
2015-05-31 15:40:22 +0900

    init base

--- lnw.template/src/main/java/com/lnw/template/controller/HomeController.java
+++ lnw.template/src/main/java/com/lnw/template/controller/HomeController.java
@@ -3,32 +3,41 @@ package com.lnw.template.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Handles requests for the application home page.
- */
+import com.lnw.template.service.HomeService;
+
+@RequestMapping(value="/base/*")
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@Autowired
+	private HomeService homeService;
+	
+	@RequestMapping(value = "getBase")
+	public void getBase(@RequestParam Map<String, Object> params,  Model model) throws Exception {
+		logger.info("계정 정보");
+		
+		//service 호출
+		model.addAttribute("list", homeService.getBaseList(params));
+	}
+	
+	@RequestMapping(value = "home")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
--- lnw.template/src/main/java/com/lnw/template/service/HomeService.java
+++ lnw.template/src/main/java/com/lnw/template/service/HomeService.java
@@ -1,26 +1,27 @@
 package com.lnw.template.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
-import com.lnw.template.database.ICommonDAO;
 
 @Service
 public class HomeService {
 	
+	/*
 	@Qualifier("commonDAO")
 	@Autowired
 	private ICommonDAO commonDAO;
+	*/
 	
 	/**
 	 * 기본 기능
 	 */
 	public List getBaseList(Map<String, Object> params) throws Exception {
-		return commonDAO.selectList("base.getBaseList", params);
+		//return commonDAO.selectList("base.getBaseList", params);
+		
+		return new ArrayList();
 	}
 	
 }
